ada0330977b5257c5052e38cba11dc013afae90c
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-04-28 01:02:55 +0630

    day 1 data

---
+++ test.php
@@ -0,0 +1,37 @@
+<?php
+
+// string "", ''
+// $name = "hlaing min than";
+// $name = 'hlaing min than';
+// echo $name;
+
+//number (int,float)
+// $number = 13.5;
+
+// echo $number;
+
+//Boolean (true,false)
+// var_dump($name);//debug
+
+// numeric array [] (index number)
+
+// $personal_infos = ['hlaing min than', 21, true, ['laptop', 'phone']];
+
+// echo "<pre>"; //html pre tag can make pretty format for array
+// print_r($personal_infos[3][1]); //multidimentional arrays
+
+// associated array
+
+// $personal_infos = [
+//     'name' =>  'hlaing min than',
+//     'age' => 21,
+//     'is_married' => true,
+//     'accesories' => ['laptop', 'phone'],
+//     'car' => 'swift'
+// ];
+
+// echo "<pre>"; //html pre tag can make pretty format for array
+// print_r($personal_infos);
+
+// $empty_bottle = null;
+// var_dump($empty_bottle);
